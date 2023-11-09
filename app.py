7468b3cffd8b0800cb689e5cac7855e39f275803
MajoCruz <148779817+MajoCruz@users.noreply.github.com>
2023-11-09 08:06:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
+
 from datetime import datetime
 import dash
 
